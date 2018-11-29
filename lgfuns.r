1db962c701a970f890778a2b75e9fd87ec7ad48e
Bernd.Gruber <Bernd.Gruber@canberra.edu.au>
2018-11-29 12:59:15 +1100

    ggmap to raster

--- lgfuns.r
+++ lgfuns.r
@@ -55,3 +55,22 @@ values(r)[is.na(values(r))==T]<- resVal
 if (plotres) plot(r)
 return(r)
 }
+
+ggmap_to_raster <- function(x){
+  if(!inherits(x, "ggmap")) ps_error("x must be a ggmap object (e.g. from ps_bbox_ggmap)")
+  
+  map_bbox <- attr(x, 'bb')
+  .extent <- raster::extent(as.numeric(map_bbox[c(2,4,1,3)]))
+  my_map <- raster::raster(.extent, nrow= nrow(x), ncol = ncol(x))
+  rgb_cols <- setNames(as.data.frame(t(col2rgb(x))), c('red','green','blue'))
+  red <- my_map
+  raster::values(red) <- rgb_cols[['red']]
+  green <- my_map
+  raster::values(green) <- rgb_cols[['green']]
+  blue <- my_map
+  raster::values(blue) <- rgb_cols[['blue']]
+  ras <- raster::stack(red,green,blue)
+  ras
+}
+
+
